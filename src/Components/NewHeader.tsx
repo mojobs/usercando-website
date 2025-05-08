5b33bccd3f2a9f87b4fcf4dca5c26b20909b78b5
mojobs <davidajibola12@gmail.com>
2025-05-08 01:30:26 +0100

    The 1024 Laptop screen size is responsive

--- src/Components/NewHeader.tsx
+++ src/Components/NewHeader.tsx
@@ -1,35 +1,69 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React from "react";
+import { Link } from "react-router-dom";
 
 const NewHeader = () => {
   return (
     <>
-    <header className="bg-background">
-        <div className='relative'>
-            <nav className='flex items-center gap-20 p-2.5 mx-auto mb-2.5 min-[350px]:gap-30 justify-between'>
-                <div className='flex gap-2 items-center justify-self-start'>
-                    <img className='' src="public/icons/logo.svg" alt="" />
-                    <p className='text-white text-[1rem]'>UserCanDo</p>
-                </div>
-                <div className='justify-self-end'>
-                    <img className='size-7' src="public/icons/icons-menu-50.svg" alt="" />
-                </div>
-            </nav>
-            <div className='px-5 line-height-letter-spacing mb-2.5'>
-                <h4 className='text-[#45DCBF] font-nunito text-[0.5rem] mb-1 leading-normal  pr-5'>Action Intelligence Understand key business actions before
-                implementing AI and Automation.</h4>
-                <h1 className='text-white font-lato text-[0.9rem] mb-1  pr-5'>Achieve Action Intelligence: The Essential <br/> Precursor to AI
-                Transformation</h1>
-                <h2 className='font-nunito text-[#45DCBF] text-[0.65rem] pr-5'>From Operating in the Dark to Full visibility of your Daily
-                Business Actions</h2>
+      <header className="bg-background">
+        <div className="">
+          <nav className="flex items-center gap-20 p-2.5 mx-auto mb-2.5 min-[350px]:gap-30 justify-between min-[1000px]:p-5 min-[1000px]:pl-10">
+            <div className="flex gap-2 items-center justify-self-start">
+              <img className="" src="public/icons/logo.svg" alt="" />
+              <p className="text-white text-[1rem]">UserCanDo</p>
             </div>
-            <div className='font-nunito text-[0.5rem] px-5 pb-2.5 relative'>
-                <button className='bg-[#45DCBF] px-2 py-1 rounded-2xl'>Let's Talk</button>
-                <button className='ml-2.5 px-2 py-1 inline-block'><Link className='text-white' to="/howitworks">How It Works</Link></button><span className='inline-block absolute top-[5px] left-36'><img className='size-2.5' src="public/icons/arrow-right.svg" alt="" /></span>
+            <div className="justify-self-end min-[1000px]:hidden">
+              <img
+                className="size-7"
+                src="public/icons/icons-menu-50.svg"
+                alt=""
+              />
             </div>
+            <div className=" max-[1000px]:hidden font-nunito font-[400px] text-[#F0FDF9] flex gap-5 items-center text-[1rem]">
+              <a href="">Home</a>
+              <Link to="/landing">How it Works</Link>
+              <Link to="/pricing">Pricing</Link>
+              <a href="">FAQ</a>
+              <Link to="/blog">Blog</Link>
+              <button className="bg-primary text-black rounded-[20px] py-[10px] px-[40px] hover:cursor-pointer hover:brightness-90">
+                Try for Free
+              </button>
+            </div>
+          </nav>
+          <div className="px-5 line-height-letter-spacing mb-2.5 min-[1000px]:pl-10 min-[1000px]:mb-5">
+            <h4 className="text-[#45DCBF] font-nunito text-[0.5rem] mb-1 leading-normal pr-5 min-[1000px]:text-[0.8rem] min-[1000px]:font-[400px] min-[1000px]:mb-2.5">
+              Action Intelligence Understand key business actions before
+              implementing AI and Automation.
+            </h4>
+            <h1 className="text-white font-lato text-[0.9rem] mb-1  pr-5 min-[1000px]:text-[2.25rem] min-[1000px]:font-[700px] min-[1000px]:leading-[100%] min-[1000px]:tracking-[0%] min-[1000px]:mb-4">
+              Achieve Action Intelligence: The Essential <br /> Precursor to AI
+              Transformation
+            </h1>
+            <h2 className="font-nunito text-[#45DCBF] text-[0.65rem] pr-5 min-[1000px]:text-[1rem]">
+              From Operating in the Dark to Full visibility of your Daily
+              Business Actions
+            </h2>
+          </div>
+          <div className="font-nunito text-[0.5rem] px-5 pb-5 relative min-[1000px]:text-[1rem] min-[1000px]:pl-10 min-[1000px]:pb-12">
+            <button className="bg-[#45DCBF] px-2 py-1 rounded-2xl min-[1000px]:px-10 min-[1000px]:py-2 hover:cursor-pointer hover:brightness-90">
+              Let's Talk
+            </button>
+            <button className="ml-2.5 px-2 py-1 inline-block">
+              <Link className="text-white" to="/howitworks">
+                How It Works
+              </Link>
+            </button>
+            <span className="inline-block absolute top-[5px] left-36 min-[1000px]:left-78 min-[1000px]:top-2 hover:cursor-pointer">
+              <img
+                className="size-2.5 min-[1000px]:size-6"
+                src="public/icons/arrow-right.svg"
+                alt=""
+              />
+            </span>
+          </div>
         </div>
-        </header></>
-  )
-}
+      </header>
+    </>
+  );
+};
 
-export default NewHeader
+export default NewHeader;
